005a0b9aabdf519f24f1496c9da563c97059f929
Civilight21 <deleteduser882@gmail.com>
2024-11-12 11:35:01 -0500

    Update lab11.sql

--- lab11.sql
+++ lab11.sql
@@ -1,1 +1,11 @@
+-- Task 1
+SELECT title, due_date
+FROM assignments
+WHERE status != 'Completed'	
+  AND course_id LIKE 'COMP1234';
+
+-- Task 2
+SELECT course_id, title, status,min(due_date) FROM assignments;
+
+--Task 3
 
